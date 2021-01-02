0591f71620cfd3a9cee2a5982795cc5f287461ea
Jon Detka <jdetka@ucsc.edu>
2021-01-01 16:35:39 -0800

    Update DHT-11_FC-37_PCF8523_Sleep_RTC_vers.1.1_010121.ino

--- DHT-11_FC-37_PCF8523_Sleep_RTC_vers.1.1_010121.ino
+++ DHT-11_FC-37_PCF8523_Sleep_RTC_vers.1.1_010121.ino
@@ -1,26 +1,34 @@
-/* Power saving code, wakes on RTC-based interrupt
- * 12-31-20 CDW
- * code based on tutorials at https://thekurks.net/blog/2018/1/24/guide-to-arduino-sleep-mode
- * RTC setup based on example from Adafruit RTClib: pcf8523Countdown
+/* 
+ * Power saving code implemented for , Plant Disease Ecology weather datalogger wakes on RTC-based interrupt and logs 
+ * leaf wetness, air temperature, and relative humidity to SD card. 
+ * Development continuing to include tally of rain bucket tips from standard mesh fog collector. 
+ * 12-31-20 CDW power-saving interrupt code development and 1-1-21 JRD sensor logging and SD storage. 
+ * 
+ * Code based on tutorials at: 
+ * https://thekurks.net/blog/2018/1/24/guide-to-arduino-sleep-mode 
+ * https://learn.adafruit.com/adafruit-data-logger-shield
+ * https://www.arduino.cc/en/Tutorial/LibraryExamples/Datalogger
+ * 
+ * RTC setup based on example from Adafruit RTClib: pcf8523Countdown and Arduino Uno R3. 
  * RTC is provided by Adafruit datalogger shield with PCF8523
- * INT/SQW pin from RTC shield must be connected to arduino pin 2 using jumper wire to trigger the interrupt
+ * Important Note: INT/SQW pin from RTC shield must be connected to arduino pin 2 using jumper wire to trigger the interrupt. 
  */
 
 //Libraries
-#include <avr/sleep.h>//this AVR library contains the methods that controls the sleep modes
-#include <SD.h>
-#include <dht.h>
-#include <RTClib.h> // When this is not included I get error: 'RTC_PCF8523' does not name a type
+#include <avr/sleep.h> // methods that controls the sleep modes
+#include <SD.h>        // methods for accessing SD card on shield. 
+#include <dht.h>       // DHT-11 Temperature and Relative Humidity sensor libraries 
+#include <RTClib.h>    // When this is not included I get error: 'RTC_PCF8523' does not name a type
 
 RTC_PCF8523 rtc; //make the RTC object
 const int chipSelect = 10; // Set to pin 10 for Arduino R3 Uno to connect with Adafruit shield. 
 
-//Pin definitions
-//Note that on Aruino Uno, pins 2 and 3 are HW interrupt capable
+// Pin Definitions
+// Note that on Aruino Uno, pins 2 and 3 are HW interrupt capable and can be connected to the Adafruit SD datalogger shield with RTC SQ pin. 
 #define rtcInterruptPin 2 //Pin we are going to use to wake up the Arduino from the RTC
 #define counterInterruptPin 3 // Pin we are going to use to count pulses
 
-// Define temperature & relative humidity sensor DHT_11
+// Define temperature & relative humidity sensor DHT-11
 dht DHT; 
 #define dhtPin 4       // Data Digital Pin 4 
 #define dhtPower 5     // Data Digital Pin 3
@@ -53,14 +61,14 @@ void setup() {
     Serial.flush();
     abort();
   }
-    // Timer configuration is not cleared on an RTC reset due to battery backup!
+  // Timer configuration is not cleared on an RTC reset due to battery backup!
   rtc.deconfigureAllTimers();
 
   // Uncomment an example below to configure RTC interrupt:
 
   // rtc.enableCountdownTimer(PCF8523_FrequencyHour, 24);    // 1 day
-  // rtc.enableCountdownTimer(PCF8523_FrequencyMinute, 150); // 2.5 hours
-  rtc.enableCountdownTimer(PCF8523_FrequencySecond, 600);     // 10 minute
+     rtc.enableCountdownTimer(PCF8523_FrequencyMinute, 10); // 10 minutes
+  // rtc.enableCountdownTimer(PCF8523_FrequencySecond, 1);     // 1 minute
   // rtc.enableCountdownTimer(PCF8523_Frequency64Hz, 32);    // 1/2 second
   // rtc.enableCountdownTimer(PCF8523_Frequency64Hz, 16);    // 1/4 second
   // rtc.enableCountdownTimer(PCF8523_Frequency4kHz, 205);   // 50 milliseconds
@@ -71,17 +79,17 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(counterInterruptPin), countISR, RISING); // Uno interrupt 1 is pin 3. Attach interrupt for the voltage on this pin changing to high.
   digitalWrite(LED_BUILTIN,HIGH);//turning LED on 
 
-// Added these for sensors and SD card (SD card on pin 10). 
+// Added these for sensors and SD card (SD card on pin 10 of Arduino Uno R3). 
   pinMode(2, INPUT);
   digitalWrite(2,HIGH);
   
-  pinMode(leafPower,OUTPUT);
-  pinMode(dhtPower, OUTPUT);
+  pinMode(leafPower,OUTPUT);    // FC-37  power
+  pinMode(dhtPower, OUTPUT);    // DHT-11 power
   digitalWrite(leafPower, LOW); 
   digitalWrite(dhtPower, LOW); 
 
 // inserted SD card write here 
-  pinMode(10, OUTPUT);  // default chip select pin is set to output, even if you don't use it:
+  pinMode(10, OUTPUT);  // default chip select pin is set to output, even if you don't use it: requires defining const int chipSelect = 10;
 
  while (!Serial);
 
@@ -89,11 +97,11 @@ void setup() {
 
   if (!SD.begin(chipSelect)) {
 
-    Serial.println("initialization failed. Things to check:");
-    Serial.println("1. is a card inserted?");
-    Serial.println("2. is your wiring correct?");
-    Serial.println("3. did you change the chipSelect pin to match your shield or module?");
-    Serial.println("Note: press reset or reopen this serial monitor after fixing your issue!");
+    Serial.println("Initialization failed. Here's some things to check:");
+    Serial.println("1. Is an SD card inserted with the file 'datalog.txt'?");
+    Serial.println("2. Is your wiring correct? (especially the interrupt");
+    Serial.println("3. Is the chipSelect pin matching your shield or module?");
+    Serial.println("Press reset or reopen this serial monitor after fixing the issue!");
 
     while (true); 
   }
@@ -102,20 +110,18 @@ void setup() {
 
 }
   
-
 void loop() {
  // if the loop is running, it's because we're awake.  This is because an interrupt fired
  // if timer has gone off, tally data and log to SD card
  // if counter interrupt has fired, increment counter tally and go back to sleep 
  if (g_timerFlag) { //if timer flag has fired, do all the sensor reads and SD card log
 
-logfile = SD.open("datalog.txt", FILE_WRITE); 
+logfile = SD.open("datalog.txt", FILE_WRITE); // Open the file datalog.txt on the SD card. Requires manually uploading for now. 
 
 // if the file is available, write to it:
-
 if (logfile) {
 
-// Get the RTC time from PCF8523
+// Get the RTC time from PCF8523 and log it to SD logfile.print
 DateTime now;
  
 now = rtc.now(); 
@@ -146,20 +152,18 @@ now = rtc.now();
   Serial.print(now.second(), DEC);
   Serial.println();
   
-// Get data from DHT-11 using dhtRead()
- int dhtData = dhtRead();    // Read DHT sensor
- int val = readSensor(); // Take leaf wetness reading 
+// Get data from DHT-11 using dhtRead()function. See lines at end for dhtRead() function. 
+ int dhtData = dhtRead();    // Read DHT-11 sensor for temperature and relative humidity
+ int val = readSensor(); // Take leaf wetness reading from the FC-37
  
-// Determine status of leaf wetness sensor FC-37 
+// Determine categorical status of digital logging leaf wetness sensor FC-37 
   if (val) {
     Serial.print("Dry");
-   
     logfile.print("Dry");
     logfile.print(", "); 
     
   } else {
     Serial.print("Wet");
-    
     logfile.print("Wet");
     logfile.print(",");
   }
@@ -175,17 +179,17 @@ now = rtc.now();
   Serial.print(DHT.humidity);
   Serial.print(",");
 
-  //display current count of pulse counter
+  //display current count of pulse counter for attachment to rain tipping bucket - standard fog collector. 
   logfile.print(g_count);
   Serial.print("count = ");
   Serial.println(g_count);
 
-logfile.close();
+logfile.close(); // close the file when done logging data. 
 
  }
-   g_count = 0; //reset count for next period
+   g_count = 0; //reset count for next sampling period
  
-  delay(2000);
+  delay(2000);  // Give it a couple of seconds to close file and reset counter. 
 
 // End of logging time and sensor data. 
   
@@ -199,7 +203,7 @@ else {
 
   }
 
- delay(2000); //wait 0.1 second before going to sleep
+ delay(2000); //wait a couple of second before going to sleep. Required I think because DHT-11 is sloooow. 
  Going_To_Sleep(); //go back to sleep and wait for next event to be triggered by an interrupt.  We put it to sleep here regardless of what woke up the processor
 }
 
@@ -228,8 +232,7 @@ void countISR(){ // Interrupt service request for when counter pulses
   //note that this interrupt will wake up the arduino.  We rely on the main loop to put it back to sleep.
 }
 
-//  Function to wake-up leaf wetness sensor and return digital value (0 = wet, 1 = dry). 
-int readSensor() {
+int readSensor() { //  Function to wake-up leaf wetness sensor and return digital value (0 = wet, 1 = dry). Reduces corrosion build-up
   digitalWrite(leafPower, HIGH);  // Turn the sensor ON
   delay(10);              // Allow power to settle
   int val = digitalRead(leafPin); // Read the sensor output
@@ -237,8 +240,7 @@ int readSensor() {
   return val;             // Return the value
 }
 
-//  Function to return DHT-11 Temperature & RH digital values
-int dhtRead() {
+int dhtRead() { //  Function to return DHT-11 Temperature & RH digital values. 
   digitalWrite(dhtPower, HIGH);     // Turn the sensor ON
   delay(2000);                      // Allow time for sensor to collect. 
   int dhtData = DHT.read11(dhtPin); // Read the sensor output
